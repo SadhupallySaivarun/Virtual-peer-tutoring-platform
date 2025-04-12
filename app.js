9e14a73fbe8c2fff5cdab7315a6d8d282c5e430b
SadhupallySaivarun <sadhupallysaivarun@gmail.com>
2025-04-12 15:48:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+<!-- Add Firebase libraries -->
+<script src="https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js"></script>
